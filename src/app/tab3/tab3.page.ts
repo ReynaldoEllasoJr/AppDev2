a8e57c1ef8add853b5297399f181c94ac9a7e687
ReynaldoEllasoJr <you@example.com>
2022-03-29 03:17:34 +0800

    Content on my activity 2

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  contactlist = [
+    {id: 1, name: 'Reynaldo B. Ellaso Jr', email: 'ellasoreyn01@gmail.com'  ,number: '09109679392'},
+    {id: 2, name: 'Sanix Metrivs', email: 'sanix213##@gmail.com'  ,number: '09123456789'},
+    {id: 3, name: 'Janine Urmeneta', email: 'janineurmenta@gmail.com'  ,number: '093054784555'},
+   
+  ]
 
   constructor() {}
 
